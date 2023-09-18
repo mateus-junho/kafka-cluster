1819bc68ffce17d16d9c5a457f333979ce7c13c7
Mateus Junho <mateus.junho@farfetch.com>
2023-09-18 21:19:44 +0100

    feat: add project consumer and project producer

--- src/Consumer/Program.cs
+++ src/Consumer/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "devio",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+consumer.Subscribe("topic_test");
+
+while(true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"{result.Message.Key}-{result.Message.Value}");
+}
